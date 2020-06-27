0867c4081f1970d0f2f9b9ed3fd8e93289aa34fa
Doğan EFE <doganefe2@outlook.com>
2020-06-27 14:32:36 +0300

    Updates

---
+++ precache-manifest.2b05c790d063c8399c4fa9509e0f512c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e2ce43c209cf55c760975127bc196d",
+    "url": "/urgerapp/index.html"
+  },
+  {
+    "revision": "88d51513f61e137e4f01",
+    "url": "/urgerapp/static/css/main.b3a9b1f4.chunk.css"
+  },
+  {
+    "revision": "4de5e04d43258dac979c",
+    "url": "/urgerapp/static/js/2.54b05c25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/urgerapp/static/js/2.54b05c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d51513f61e137e4f01",
+    "url": "/urgerapp/static/js/main.4beac589.chunk.js"
+  },
+  {
+    "revision": "08dec799431bd9b7e2dd",
+    "url": "/urgerapp/static/js/runtime-main.d3d7ca69.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/urgerapp/static/media/original.b8503d26.png"
+  }
+]);
